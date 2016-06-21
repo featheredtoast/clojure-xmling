f568dfff0ae16caff76fd9e43954452997d1ccef
Jeff Wong <awole20@gmail.com>
2016-06-21 01:25:00 -0700

    work in progress - xmlns is supported in beta
    ...however, tag= on data.xip needs exact name (including ns)

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.xml "0.0.8"]
+                 [org.clojure/data.xml "0.1.0-beta1"]
                  [org.clojure/data.zip "0.1.1"]])
--- src/xmling/core.clj
+++ src/xmling/core.clj
@@ -13,23 +13,30 @@
   (zip-xml/xml-> xml-root
                  clojure.data.zip/descendants
                  :source))
+(defn ns-tag=
+  [tag]
+  (fn [loc]
+    true
+    #_(= tag (xml/qname-local (:tag loc)))))
 (defn edit-source
   "do some edits!"
   [source]
-  (zip/edit source #(assoc-in % [:content] (str "edited..." (zip-xml/text source)))))
+  (println source)
+  #_(zip/edit source #(assoc-in % [:content] (str "edited..." (zip-xml/text source)))))
 (defn read-xliff
   "Reads xliff!"
   [file]
   (let [input (clojure.java.io/reader (clojure.java.io/resource file))
         root (zip/xml-zip (xml/parse input))
-        sources (get-all-sources root)
-        edited-source (edit-source (first sources))
-        new-root (zip/root edited-source)]
-    new-root))
+        xmlns (xml/find-xmlns root)
+        source (zip-xml/xml1-> root clojure.data.zip/descendants (ns-tag= "source"))
+        edited-source (edit-source source)]
+    (println xmlns)
+    root))
 
 ;; note this forgets its root namespace. xmlns is not present in the output...
-(xml/emit-str (read-xliff "test.xliff"))
+(xml/indent-str (read-xliff "test.xliff"))
 ;; emit-str would fail for this, despite only adding namespaces to xml:lang
-(read-xliff "test-bad.xliff")
+(println (xml/indent-str (read-xliff "test-bad.xliff")))
 
 
