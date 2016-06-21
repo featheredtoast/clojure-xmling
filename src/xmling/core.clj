2c4b38f52f3687ba82bf4188e1c046e6f0179d3b
Jeff Wong <awole20@gmail.com>
2016-06-20 23:23:30 -0700

    wtf is with this namespace cruft.
    ...namespaces look like their handled not well...

--- src/xmling/core.clj
+++ src/xmling/core.clj
@@ -3,19 +3,28 @@
             [clojure.zip :as zip]
             [clojure.data.zip.xml :as zip-xml]))
 
-(defn get-source-element
+(defn get-source-text
   "given a source element, get its stuff?"
   [e]
   (zip-xml/text e))
+(defn get-all-sources
+  "get all source elements"
+  [xml-root]
+  (zip-xml/xml-> xml-root
+                 clojure.data.zip/descendants
+                 :source))
+(defn edit-source
+  "do some edits!"
+  [source]
+  (zip/edit source #(assoc-in % [:content] (str "edited..." (zip-xml/text source)))))
 (defn read-xliff
   "Reads xliff!"
   []
   (let [input (clojure.java.io/reader (clojure.java.io/resource "test.xliff"))
         root (zip/xml-zip (xml/parse input))
-        sources (mapv get-source-element
-                      (zip-xml/xml-> root
-                                      clojure.data.zip/descendants
-                                      :source))]
-    sources))
+        sources (get-all-sources root)
+        edited-source (edit-source (first sources))
+        new-root (zip/root edited-source)]
+    new-root))
 
-(read-xliff)
+(xml/emit-str (read-xliff))
